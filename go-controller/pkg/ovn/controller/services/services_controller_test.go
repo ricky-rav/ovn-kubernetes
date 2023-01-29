56c1a8322f18ac78b3dcf96c0a4c09f703516592
Flavio Fernandes <flaviof@redhat.com>
2023-01-29 01:32:27 +0000

    services controller: make ensureLbs use top level cache
    This commit modifies the sync services function to use the top
    level cache (aka alreadyApplied) instead of the second level
    cache (aka lb_cache).
    
    Reported-at: https://issues.redhat.com/browse/OCPBUGS-6739
    Signed-off-by: Flavio Fernandes <flaviof@redhat.com>
    (cherry picked from commit 2000f2e057267414d5c639326e31f5472cb39f06)
    Signed-off-by: Yun Zhou <yunz@nvidia.com>

--- go-controller/pkg/ovn/controller/services/services_controller_test.go
+++ go-controller/pkg/ovn/controller/services/services_controller_test.go
@@ -64,6 +64,7 @@ func newControllerWithDBSetup(dbSetup libovsdbtest.TestSetup) (*serviceControlle
 	}
 	controller.servicesSynced = alwaysReady
 	controller.endpointSlicesSynced = alwaysReady
+	controller.initTopLevelCache()
 	return &serviceController{
 		controller,
 		informerFactory.Core().V1().Services().Informer().GetStore(),
@@ -460,7 +461,7 @@ func TestSyncServices(t *testing.T) {
 
 			err = controller.syncService(ns + "/" + serviceName)
 			if err != nil {
-				t.Errorf("syncServices error: %v", err)
+				t.Fatalf("syncServices error: %v", err)
 			}
 
 			g.Expect(controller.nbClient).To(libovsdbtest.HaveData(tt.expectedDb))
