e646b167950d6b2648a2bd04d8b82ab6b38dc9b8
Zhen Wang <zhewang@nvidia.com>
2023-10-12 15:28:17 -0700

    SDN-2338 ovnkube-node ovnkube-node-dpu-host need liveness check for ovn-cni-server.sock
    Signed-off-by: Zhen Wang <zhewang@nvidia.com>

---
+++ dist/images/ovnkube-node-liveness-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl --unix-socket /var/run/ovn-kubernetes/cni/ovn-cni-server.sock -X POST http://dummy/ > /dev/null 2>err.txt
+if [ $? -ne 0 ]; then
+    echo "ERROR: ovn-cni-server.sock is not working"
+    echo "$(< /root/err.txt)"
+    exit 1
+fi
+
+exit 0
