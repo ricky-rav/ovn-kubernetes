9f45c3cc21ae8b33defd76d84d831b4a354be0c6
Zhen Wang <zhewang@nvidia.com>
2023-10-12 15:50:34 -0700

    [SDN-2323] Check for cni conf file during readiness probes
    Signed-off-by: nithyar nithyar@nvidia.com

--- dist/images/ovnkube-node-liveness-check.sh
+++ dist/images/ovnkube-node-liveness-check.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+file="/etc/cni/net.d/10-ovn-kubernetes.conf"
+if [[ ! -f ${file} ]] ; then
+    echo "ERROR: ${file} does not exist"
+    exit 1
+fi
+
 curl --unix-socket /var/run/ovn-kubernetes/cni/ovn-cni-server.sock -X POST http://dummy/ > /dev/null 2>err.txt
 if [ $? -ne 0 ]; then
     echo "ERROR: ovn-cni-server.sock is not working"
