1b2ea9ee5df03c17c8ba9514451f07963075b904
Lei Huang <leih@nvidia.com>
2024-06-28 13:58:56 -0700

    [SDN-2847] prevent concurrent overwrites of external_ids:ovn-bridge-mappings
    The NAD controller may have multiple workers that call function
    `SecondaryLocalnetNodeNetworkController.updateLocalnetOvnBridgeMapping()`
    concurrently. A mutex is required to prevent concurrent modifications to
    `external_ids:ovn-bridge-mappings`, ensuring that updates do
    not overwrite each other.
    
    (cherry picked from commit c4935f64e92abd8fb41cd712e3924e1b96ef4b04)
    Signed-off-by: Yun Zhou <yunz@nvidia.com>

--- go-controller/pkg/node/secondary_localnet_node_network_controller.go
+++ go-controller/pkg/node/secondary_localnet_node_network_controller.go
@@ -239,7 +239,13 @@ func (nc *SecondaryLocalnetNodeNetworkController) Cleanup(netName string) error 
 	return nil
 }
 
+var ovsMutex = sync.Mutex{}
+
 func (nc *SecondaryLocalnetNodeNetworkController) updateLocalnetOvnBridgeMapping(toAdd bool) error {
+	// The NAD controller may have multiple workers that call this function concurrently.
+	// A mutex is required to prevent concurrent overwrites of external_ids:ovn-bridge-mappings.
+	ovsMutex.Lock()
+	defer ovsMutex.Unlock()
 	bridgeName := ""
 	if toAdd {
 		// ngn-localnet-bridge-mappings exernal_ids is in the form of "<network_prefix1>:<br1>,<network_prefix2>:<br2>...".
