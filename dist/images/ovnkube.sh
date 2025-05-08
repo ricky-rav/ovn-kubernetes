28cf66bc6d340570cf382137791a22ae9ba4b74f
Flavio Fernandes <ffernandes@nvidia.com>
2025-05-08 21:04:04 +0000

    ovnkube.sh: use node name as zone as default if ovn-ic
    If the `k8s.ovn.org/zone-name` label is not set on the node,
    the fallback logic now uses the node name as the zone when
    `ovn_enable_interconnect` is true. Otherwise, the zone
    defaults to "global" as before.
    
    Also updated the empty string check to use `-z`, which is
    more idiomatic in Bash.
    
    Signed-off-by: Flavio Fernandes <ffernandes@nvidia.com>
    (cherry picked from commit fea6c67c45629196684876e7f12ee4b87061b754)
    Signed-off-by: Yun Zhou <yunz@nvidia.com>

--- dist/images/ovnkube.sh
+++ dist/images/ovnkube.sh
@@ -948,11 +948,12 @@ function memory_trim_on_compaction_supported {
 }
 
 function get_node_zone() {
-  zone="global"
-  if [ "${OVN_ENABLE_INTERCONNECT}" == "true" ]; then
-    zone=$(kubectl --subresource=status --server=${K8S_APISERVER} --token=${k8s_token} --certificate-authority=${K8S_CACERT} \
-      get node ${K8S_NODE} -o=jsonpath={'.metadata.labels.k8s\.ovn\.org/zone-name'})
-    if [ "$zone" == "" ]; then
+  zone=$(kubectl --subresource=status --server=${K8S_APISERVER} --token=${k8s_token} --certificate-authority=${K8S_CACERT} \
+     get node ${K8S_NODE} -o=jsonpath={'.metadata.labels.k8s\.ovn\.org/zone-name'})
+  if [ -z "$zone" ]; then
+    if [[ ${ovn_enable_interconnect} == "true" ]]; then
+      zone="${K8S_NODE}"
+    else
       zone="global"
     fi
   fi
